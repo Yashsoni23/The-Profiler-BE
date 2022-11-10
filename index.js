790f835724efd624a95e75def143844a0222f5fb
Yash simejiya <107744977+Yashsoni23@users.noreply.github.com>
2022-11-11 01:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,12 @@ const server = http.createServer(app);
 const bodyParser = require('body-parser');
 const cors = require("cors")
 const fileUpload = require("express-fileupload");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 dotenv.config({ path: '/config.env' });
 
 
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static("FE/build"))
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'FE', 'build', 'index.html')
-        )
-    })
-
-}
 app.listen(PORT, () => {
     console.log("app started");
 });
